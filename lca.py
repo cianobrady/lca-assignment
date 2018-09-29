2bd52dd463f3ea58c9fa509c1adb6cd6be068f87
cianobrady <cianobrady7@gmail.com>
2018-09-29 23:34:42 +0100

    added class and constructor for Node. Outlined API for lca.py and added sample code in order to test running the unit tests.

--- lca.py
+++ lca.py
@@ -1,1 +1,20 @@
-print("This is a test")
+class Node:
+    # Node constructor
+    def __init__(self, key):
+        self.key = key
+        self.right = None
+        self.left = None
+		
+def findLCA(root, node1, node2):
+    #sample code to test python unit testing
+	if(not (contains(node1) and contains(node2))):
+		return -1
+		
+	return root.key
+	 
+def contains(key):
+    #sample code to test python unit testing
+	if(node >= 0 and node <=8):
+		return True
+	return False
+	
