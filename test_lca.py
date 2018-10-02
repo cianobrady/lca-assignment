670453b6278aae630376aefcae746c5a5eaa99c4
cianobrady <cianobrady7@gmail.com>
2018-10-02 19:38:35 +0100

    added more tests to test working solution

--- test_lca.py
+++ test_lca.py
@@ -4,9 +4,17 @@ import lca
 class TestLCA(unittest.TestCase):
 
     def test_findLCA(self):
+    
+        #test if no tree
+        node = None
+        self.assertEqual(lca.findLCA(node, 4, 3), -1)
+        
         # Creating a binary tree
         node = lca.Node(0)
-
+        
+        #test root only tree
+        self.assertEqual(lca.findLCA(node, 0, 0), 0)
+        
         node.left = lca.Node(6)
         node.right = lca.Node(2)
 
@@ -24,8 +32,14 @@ class TestLCA(unittest.TestCase):
         self.assertEqual(lca.findLCA(node, 2, 1), 2)
         #test error scenario: node not in binary tree
         self.assertEqual(lca.findLCA(node, 2, 9), -1)
+        #test error scenario: non integer node
+        self.assertEqual(lca.findLCA(node, "a", 9), -1)
 		
     def test_contains(self):
+        #test if no tree
+        node = None
+        self.assertEqual(lca.contains(node, 0), False)
+        
         # Creating a binary tree
         node = lca.Node(0)
 
@@ -46,3 +60,32 @@ class TestLCA(unittest.TestCase):
         self.assertEqual(lca.contains(node, 2), True)
         #test key not in tree
         self.assertEqual(lca.contains(node, 9), False)
+        
+    def test_path(self):
+        #test if no tree
+        node = None
+        self.assertEqual(lca.path(node, 0), [])
+        
+        # Creating a binary tree
+        node = lca.Node(0)
+
+        node.left = lca.Node(6)
+        node.right = lca.Node(2)
+
+        node.left.left = lca.Node(4)
+        node.left.right = lca.Node(3)
+        node.right.left = lca.Node(1)
+        node.right.right = lca.Node(7)
+
+        node.left.right.right = lca.Node(5)
+        node.left.right.right.left = lca.Node(8)
+        
+        #test root
+        self.assertEqual(lca.path(node, 0), [0])
+        #test key in tree
+        self.assertEqual(lca.path(node, 2), [0, 2])
+        self.assertEqual(lca.path(node, 8), [0, 6, 3, 5, 8])
+        #test key not in tree
+        self.assertEqual(lca.path(node, 9), [])
+        
+        
