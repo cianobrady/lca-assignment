a362f037cb6e215a67c425a19aeeecc46a940f80
Cian O'Brady <cianobrady7@gmail.com>
2018-10-09 12:30:07 +0100

    added line of comment to test branching

--- test_lca.py
+++ test_lca.py
@@ -1,20 +1,20 @@
 import unittest
 import lca
-
+#comment added to test branching
 class TestLCA(unittest.TestCase):
 
     def test_findLCA(self):
-    
+
         #test if no tree
         node = None
         self.assertEqual(lca.findLCA(node, 4, 3), -1)
-        
+
         # Creating a binary tree
         node = lca.Node(0)
-        
+
         #test root only tree
         self.assertEqual(lca.findLCA(node, 0, 0), 0)
-        
+
         node.left = lca.Node(6)
         node.right = lca.Node(2)
 
@@ -25,7 +25,7 @@ class TestLCA(unittest.TestCase):
 
         node.left.right.right = lca.Node(5)
         node.left.right.right = lca.Node(8)
-		
+
         #test nodes with same parent
         self.assertEqual(lca.findLCA(node, 4, 3), 6)
         #test nodes with different parent
@@ -34,12 +34,12 @@ class TestLCA(unittest.TestCase):
         self.assertEqual(lca.findLCA(node, 2, 9), -1)
         #test error scenario: non integer node
         self.assertEqual(lca.findLCA(node, "a", 9), -1)
-		
+
     def test_contains(self):
         #test if no tree
         node = None
         self.assertEqual(lca.contains(node, 0), False)
-        
+
         # Creating a binary tree
         node = lca.Node(0)
 
@@ -53,19 +53,19 @@ class TestLCA(unittest.TestCase):
 
         node.left.right.right = lca.Node(5)
         node.left.right.right = lca.Node(8)
-        
+
         #test root
         self.assertEqual(lca.contains(node, 0), True)
         #test key in tree
         self.assertEqual(lca.contains(node, 2), True)
         #test key not in tree
         self.assertEqual(lca.contains(node, 9), False)
-        
+
     def test_path(self):
         #test if no tree
         node = None
         self.assertEqual(lca.path(node, 0), [])
-        
+
         # Creating a binary tree
         node = lca.Node(0)
 
@@ -79,7 +79,7 @@ class TestLCA(unittest.TestCase):
 
         node.left.right.right = lca.Node(5)
         node.left.right.right.left = lca.Node(8)
-        
+
         #test root
         self.assertEqual(lca.path(node, 0), [0])
         #test key in tree
@@ -87,5 +87,3 @@ class TestLCA(unittest.TestCase):
         self.assertEqual(lca.path(node, 8), [0, 6, 3, 5, 8])
         #test key not in tree
         self.assertEqual(lca.path(node, 9), [])
-        
-        
