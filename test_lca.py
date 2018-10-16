681ae22f51844acdf05b46dc10fa9149b370b5b4
Cian O'Brady <cianobrady7@gmail.com>
2018-10-16 22:38:03 +0100

    edited test cases to fit DAG

--- test_lca.py
+++ test_lca.py
@@ -6,84 +6,38 @@ class TestLCA(unittest.TestCase):
     def test_findLCA(self):
 
         #test if no tree
-        node = None
-        self.assertEqual(lca.findLCA(node, 4, 3), -1)
+        graph = None
+        self.assertEqual(lca.findLCA(graph, 4, 3), -1)
 
-        # Creating a binary tree
-        node = lca.Node(0)
+        # Creating a DAG
+        graph = lca.Graph()
 
         #test root only tree
-        self.assertEqual(lca.findLCA(node, 0, 0), 0)
+        graph.addVertex(0)
+        self.assertEqual(lca.findLCA(graph, 0, 0), 0)
 
-        node.left = lca.Node(6)
-        node.right = lca.Node(2)
+        graph.addVertex(0)
+        graph.addVertex(1)
+        graph.addVertex(2)
+        graph.addVertex(3)
+        graph.addVertex(4)
+        graph.addVertex(5)
+        graph.addVertex(6)
 
-        node.left.left = lca.Node(4)
-        node.left.right = lca.Node(3)
-        node.right.left = lca.Node(1)
-        node.right.right = lca.Node(7)
+        graph.addEdge(6, 3)
+        graph.addEdge(6, 5)
+        graph.addEdge(3, 2)
+        graph.addEdge(5, 4)
+        graph.addEdge(2, 1)
+        graph.addEdge(4, 1)
+        graph.addEdge(1, 0)
 
-        node.left.right.right = lca.Node(5)
-        node.left.right.right = lca.Node(8)
 
         #test nodes with same parent
-        self.assertEqual(lca.findLCA(node, 4, 3), 6)
+        self.assertEqual(lca.findLCA(graph, 2, 4), 6)
         #test nodes with different parent
-        self.assertEqual(lca.findLCA(node, 2, 1), 2)
+        self.assertEqual(lca.findLCA(graph, 0, 1), 1)
         #test error scenario: node not in binary tree
-        self.assertEqual(lca.findLCA(node, 2, 9), -1)
+        self.assertEqual(lca.findLCA(graph, 2, 9), -1)
         #test error scenario: non integer node
-        self.assertEqual(lca.findLCA(node, "a", 9), -1)
-
-    def test_contains(self):
-        #test if no tree
-        node = None
-        self.assertEqual(lca.contains(node, 0), False)
-
-        # Creating a binary tree
-        node = lca.Node(0)
-
-        node.left = lca.Node(6)
-        node.right = lca.Node(2)
-
-        node.left.left = lca.Node(4)
-        node.left.right = lca.Node(3)
-        node.right.left = lca.Node(1)
-        node.right.right = lca.Node(7)
-
-        node.left.right.right = lca.Node(5)
-        node.left.right.right = lca.Node(8)
-
-        #test root
-        self.assertEqual(lca.contains(node, 0), True)
-        #test key in tree
-        self.assertEqual(lca.contains(node, 2), True)
-        #test key not in tree
-        self.assertEqual(lca.contains(node, 9), False)
-
-    def test_path(self):
-        #test if no tree
-        node = None
-        self.assertEqual(lca.path(node, 0), [])
-
-        # Creating a binary tree
-        node = lca.Node(0)
-
-        node.left = lca.Node(6)
-        node.right = lca.Node(2)
-
-        node.left.left = lca.Node(4)
-        node.left.right = lca.Node(3)
-        node.right.left = lca.Node(1)
-        node.right.right = lca.Node(7)
-
-        node.left.right.right = lca.Node(5)
-        node.left.right.right.left = lca.Node(8)
-
-        #test root
-        self.assertEqual(lca.path(node, 0), [0])
-        #test key in tree
-        self.assertEqual(lca.path(node, 2), [0, 2])
-        self.assertEqual(lca.path(node, 8), [0, 6, 3, 5, 8])
-        #test key not in tree
-        self.assertEqual(lca.path(node, 9), [])
+        self.assertEqual(lca.findLCA(graph, "a", 9), -1)
