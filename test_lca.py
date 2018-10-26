3233ff7cf798e156b4ec2d69a09964eb03f24b02
Cian O'Brady <cianobrady7@gmail.com>
2018-10-26 23:16:16 +0100

    small fixess to tests

--- test_lca.py
+++ test_lca.py
@@ -6,22 +6,27 @@ class TestLCA(unittest.TestCase):
     def test_findLCA(self):
 
         #test if no tree
-        graph = None
-        self.assertEqual(lca.findLCA(graph, "B", "A"), -1)
+        #graph = None
+        #root = None
+        #self.assertEqual(lca.findLCA(graph, root, "B", "A"), -1)
 
         # Creating a DAG
-        graph = {"A" : []}
+        graph = {"G" : []}
+        root = "G"
         #test root only tree
-        self.assertEqual(lca.findLCA(graph, "A", "A"), "A")
+        self.assertEqual(lca.findLCA(graph, root, "G", "G"), "G")
 
         graph = { "A" : [], "B" : ["A"], "C" : ["B"], "D" : ["C"], "E" : ["B"], "F" : ["E"], "G" : ["D", "F"] }
 
 
         #test nodes with same parent
-        self.assertEqual(lca.findLCA(graph, "D", "F"), "G")
+        self.assertEqual(lca.findLCA(graph, root, "D", "F"), "G")
         #test nodes with different parent
-        self.assertEqual(lca.findLCA(graph, "A", "B"), "B")
+        self.assertEqual(lca.findLCA(graph, root, "A", "B"), "B")
         #test error scenario: node not in binary tree
-        self.assertEqual(lca.findLCA(graph, "C", "H"), -1)
+        self.assertEqual(lca.findLCA(graph, root, "C", "H"), -1)
         #test error scenario: non string node
-        self.assertEqual(lca.findLCA(graph, "A", 9), -1)
+        self.assertEqual(lca.findLCA(graph, root, "A", 9), -1)
+
+if __name__ == '__main__':
+    unittest.main()
