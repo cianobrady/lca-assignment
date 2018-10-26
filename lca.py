bbd284cbc209c2131d6f82333b25fcb788721af5
Cian O'Brady <cianobrady7@gmail.com>
2018-10-26 23:15:45 +0100

    developed path function that determines path from root to node. Need to alter function to return mutiple paths instead of one.

--- lca.py
+++ lca.py
@@ -1,15 +1,15 @@
 
 #findLCA takes in the root of a tree and two nodes and returns the LCA of the two nodes if they are both present in the tree\
-def findLCA(root, key1, key2):
+def findLCA(graph, root, key1, key2):
     #if tree does not contain key, return error
-    if( not (contains(root, key1) and contains(root, key2))):
+    if(key1 not in graph ) or (key2 not in graph):
         return -1
 
     path1 = []
     path2 = []
     #creating paths from root to given keys
-    path1 = path(root, key1)
-    path2 = path(root, key2)
+    path1 = paths(graph, root, key1)
+    path2 = paths(graph, root, key2)
 
     #verifies when paths differ and determines that the node in each path before the difference is the LCA
     i=0
@@ -19,24 +19,15 @@ def findLCA(root, key1, key2):
         i += 1
     return path1[i-1]
 
-#path takes in a root of a tree and a key within that tree and returns the path from the root to that key
-def path(root, key):
-    if not root:
-        return []
-    if root.key == key:
-        return[root.key]
-    temp = path(root.left, key)
-    if temp:
-        return [root.key] + temp
-    temp = path(root.right, key)
-    if temp:
-        return [root.key] + temp
-    return []
-
-#contains function takes in root of tree and a key and returns whether the tree contains the key
-def contains(root, key):
-    if not root:
-        return False
-    if root.key == key:
-        return True
-    return contains(root.left, key) or contains(root.right, key)
+#paths takes in a graph and two keys within that graph and returns the path from the first key to the second key
+#paths was developed using graph tutorial at https://www.python.org/doc/essays/graphs/
+def paths(graph, key1, key2, path=[]):
+    path = path + [key1]
+    if key1 == key2:
+        return path
+    for newKey in graph[key1]:
+        if newKey not in path:
+            thePath = paths(graph, newKey, key2, path)
+            if thePath:
+                return thePath
+    return None
