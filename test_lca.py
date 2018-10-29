1d09c1772fd1df0d1ba182ab52c956cdb96c0c2e
Cian O'Brady <cianobrady7@gmail.com>
2018-10-29 19:43:34 +0000

    developed more tests to test working solution

--- test_lca.py
+++ test_lca.py
@@ -6,27 +6,47 @@ class TestLCA(unittest.TestCase):
     def test_findLCA(self):
 
         #test if no tree
-        #graph = None
-        #root = None
-        #self.assertEqual(lca.findLCA(graph, root, "B", "A"), -1)
+        graph = None
+        root = None
+        self.assertEqual(lca.findLCA(graph, root, "B", "A"), -1)
 
         # Creating a DAG
-        graph = {"G" : []}
-        root = "G"
+        graph = {'G' : []}
+        root = 'G'
         #test root only tree
-        self.assertEqual(lca.findLCA(graph, root, "G", "G"), "G")
+        self.assertEqual(lca.findLCA(graph, root, 'G', 'G'), 'G')
 
-        graph = { "A" : [], "B" : ["A"], "C" : ["B"], "D" : ["C"], "E" : ["B"], "F" : ["E"], "G" : ["D", "F"] }
+        graph = { 'A' : [], 'B' : ['A'], 'C' : ['B'], 'D' : ['C'], 'E' : ['B'], 'F' : ['E'], 'G' : ['D', 'F'] }
 
 
         #test nodes with same parent
-        self.assertEqual(lca.findLCA(graph, root, "D", "F"), "G")
+        self.assertEqual(lca.findLCA(graph, root, 'D', 'F'), 'G')
         #test nodes with different parent
-        self.assertEqual(lca.findLCA(graph, root, "A", "B"), "B")
+        self.assertEqual(lca.findLCA(graph, root, 'A', 'B'), 'B')
         #test error scenario: node not in binary tree
-        self.assertEqual(lca.findLCA(graph, root, "C", "H"), -1)
+        self.assertEqual(lca.findLCA(graph, root, 'C', 'H'), -1)
         #test error scenario: non string node
-        self.assertEqual(lca.findLCA(graph, root, "A", 9), -1)
+        self.assertEqual(lca.findLCA(graph, root, 'A', 9), -1)
+
+
+    def test_shortest_path(self):
+        #test empty graph
+        graph = None
+        self.assertEqual(lca.shortest_path(graph, "B", "A"), [])
+
+        #test one node graph
+        graph = {'A' : []}
+        self.assertEqual(lca.shortest_path(graph, 'A', 'A'), ['A'])
+
+        #test graph
+        graph = { 'A' : [], 'B' : ['A'], 'C' : ['B'], 'D' : ['C'], 'E' : ['B'], 'F' : ['H'], 'G' : ['D', 'F'], 'H' : ['E'] }
+        self.assertEqual(lca.shortest_path(graph, 'G', "A"), ['G', 'D', 'C', 'B', 'A'])
+
+        #test error scenario: element not in graph
+        self.assertEqual(lca.shortest_path(graph, 'Z', 'A'), [])
+
+        #test error scenario: wrong type
+        self.assertEqual(lca.shortest_path(graph, 'A', 9), [])
 
 if __name__ == '__main__':
     unittest.main()
